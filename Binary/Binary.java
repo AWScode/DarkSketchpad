ef2864917152f68c075b5fa4618c17dfda9b75f4
annasalam <31779016+annasalam@users.noreply.github.com>
2017-12-06 15:41:33 -0800

    Add files via upload

---
+++ Binary/Binary.java
@@ -0,0 +1,41 @@
+/*
+Anna Salam
+Binary Project
+Due: 12.13.17
+
+
+this is my super super super rough draft to try and just outline what i'm gonna do and the stuff idk how to do
+*/
+
+import java.util.*;
+public class Binary {
+  public static void main(String[] args){
+    Integer.parseInt(String, int radix);
+    Integer.toBinaryString(sum);
+
+    System.out.println(""); //say something to introduce what i want user to do here
+    Scanner scan = new Scanner(System.in);
+    System.out.println(""); //say some more stuff like second set of numbers
+    Scanner scan = new Scanner(System.in);
+    System.out.println(""); //ask what we are supposed to do with it
+    //then I think i call on some method or something to do what they wanted to do with the two numbers
+    //do some more method stuff to try and get it to work
+    System.out.println(""); //the answer to the thing they want to happen
+    //NEED TO FIGURE OUT HOW TO DO THIS STUFF - MAYBE WORK WITH CHLOE ON IT
+
+    //considine says that we gonna have to use string, next line and stuff. gotta look it up
+    //also vicky is doing something super different so this probably isn't right
+  }
+
+  //make some methods here
+
+  //add method
+  //subtract method
+  //multiply method
+  //divide method
+
+
+}
+
+
+//if i use an array list need to make sure it is only up to 8 (for the 8 bits)

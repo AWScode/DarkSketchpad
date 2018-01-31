3490301d24711aa24a39beccd5b58f543ee45c02
Anna Salam <anna_salam@m013150.aw.org>
2018-01-31 13:46:56 -0800

    this is just my starting outline

---
+++ NextPrime.java
@@ -0,0 +1,18 @@
+/*
+Anna Salam
+Due: 02/02/18
+define a new method that will find the next largest prime (int x)
+*/
+
+import java.util.*;
+
+public class NextPrime{
+  public static void main(String[] args) { //this is my main method
+
+  }
+
+  public static int FindPrime() { //this is my method
+    
+  }
+
+}

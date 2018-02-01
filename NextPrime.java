21545edaa6f60db77a59ec4206a87823d8bd1e7b
Anna Salam <anna_salam@m013150.aw.org>
2018-02-01 14:22:04 -0800

    draft 2

--- NextPrime.java
+++ NextPrime.java
@@ -8,11 +8,28 @@ import java.util.*;
 
 public class NextPrime{
   public static void main(String[] args) { //this is my main method
+    System.out.println("Input a number to find the next largest prime number for.");
+    Scanner userNum = new Scanner(System.in);
+    int c = userNum.nextInt();
 
+    //output whatever is ended up in FindPrime method
   }
 
   public static int FindPrime() { //this is my method
-    
+    for (int i = userNum; i++){ //how can I run 2 loops at the same time so that i can keep increasing the one I'm dividing by
+    for (int b = 2; b <= i; b++){
+
+      if{
+        i / b == true;
+        //then keep doing loop
+      }
+      else{
+        //stop loop and output the i
+      }
+
+    }
+    }
+
   }
 
 }

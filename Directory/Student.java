306ffe82ec8d7f434c5b43a0170f07b01cfa5b52
Anna Salam <anna_salam@m013150.local>
2018-02-13 17:16:42 -0800

    draft 2

--- Directory/Student.java
+++ Directory/Student.java
@@ -2,8 +2,48 @@ public class Student{
 
   public static String name;
   public static int grade;
-  public static int age;
-  public static String superpower;
+  public static String power;
   public static String subject;
+
+  public Student(String n, int g, String p, String s){
+    this.name = n;
+    this.grade = g;
+    this.power = p;
+    this.subject = s;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getGrade(){
+    return grade;
+  }
+
+  public String getPower(){
+    return power;
+  }
+
+  public String getSubject(){
+    return subject;
+  }
+
+
+  public void setName(String n){
+    name = n;
+  }
+
+  public void setGreade(int g){
+    grade = g;
+  }
+
+  public void setPower(String p){
+    power = p;
+  }
+
+  public void setSubject(String s){
+    subject = s;
+  }
+
 
 }

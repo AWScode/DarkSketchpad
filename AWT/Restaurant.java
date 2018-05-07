9269b9d46cfa3314f5f0a7b929bd11b89d3f4403
Anna Salam <anna_salam@m013150.aw.org>
2018-05-07 14:30:28 -0700

    reorganizing

--- AWT/Restaurant.java
+++ AWT/Restaurant.java
@@ -3,8 +3,8 @@ import java.awt.*;
 
 public class Restaurant extends Frame{
 
-  private TextField tf;
-  private TextField tf2;
+  private TextField name;
+  private TextField party;
   private TextArea ta;
   private Button b;
   private TextField foods;
@@ -24,27 +24,27 @@ public class Restaurant extends Frame{
     });
 
 
-    tf = new TextField("");
-    tf.setBounds(110, 80, 150, 20);
-    add(tf);
-    tf2 = new TextField("");
-    tf2.setBounds(110, 110, 80, 20);
-    add(tf2);
+    name = new TextField("");
+    name.setBounds(110, 80, 150, 20);
+    add(name);
+    party = new TextField("");
+    party.setBounds(110, 110, 80, 20);
+    add(party);
 
     foods = new TextField("");
-    foods.setBounds(80, 150, 50, 20);
+    foods.setBounds(80, 200, 50, 20);
     add(foods);
     /*quantity = new TextField("");
     quantity.setBounds(220, 150, 50, 20);
     add(quantity);*/
 
-  /*  b = new Button("submit");
-    b.setBounds(285, 190, 100, 30);
+  b = new Button("submit");
+    b.setBounds(210, 130, 90, 30);
     this.add(b);
 
-    b.addActionListener(new ActionListener() {//says there's an error here
+    /*b.addActionListener(new ActionListener() {//says there's an error here
       public void ActionPerformed(ActionEvent e){
-        tf3.setText("Your Order"); //("your order" + quantity + food)
+        ta.setText(name + ", party of " + party);
       }
     });*/
 
@@ -56,10 +56,10 @@ public class Restaurant extends Frame{
     lab.setBounds(20, 30, 250, 30);
     this.add(lab);
     Label bel = new Label("menu");
-    bel.setBounds(470, 45, 80, 30);
+    bel.setBounds(470, 180, 80, 30);
     this.add(bel);
     Label abe = new Label("Payment Type:");
-    abe.setBounds(20, 200, 100, 30);
+    abe.setBounds(20, 250, 100, 30);
     this.add(abe);
     Label able = new Label("Your Name:");
     able.setBounds(20, 80, 100, 30);
@@ -68,10 +68,10 @@ public class Restaurant extends Frame{
     label.setBounds(20, 100, 130, 30);
     this.add(label);
     Label foodl = new Label("Food #:");
-    foodl.setBounds(20, 150, 50, 30);
+    foodl.setBounds(20, 200, 50, 30);
     this.add(foodl);
     Label quant = new Label("Quantity:");
-    quant.setBounds(150, 150, 70, 30);
+    quant.setBounds(150, 200, 70, 30);
     this.add(quant);
   }
 
@@ -87,7 +87,7 @@ public class Restaurant extends Frame{
 
   public void addChoice() {
     Choice c = new Choice();
-    c.setBounds(120, 190, 100, 50);
+    c.setBounds(120, 240, 100, 50);
     c.add("-----");
     c.add("cash");
     c.add("credit");
@@ -95,7 +95,7 @@ public class Restaurant extends Frame{
     this.add(c);
 
     Choice ch = new Choice();
-    ch.setBounds(220, 150, 70, 30);
+    ch.setBounds(220, 200, 70, 30);
     ch.add("--");
     ch.add("1");
     ch.add("2");
@@ -107,7 +107,7 @@ public class Restaurant extends Frame{
 
   public void addList() {
     List l = new List(4);
-    l.setBounds(430, 75, 120, 90);
+    l.setBounds(430, 210, 120, 90);
     l.add("1. spaghetti");
     l.add("2. tacos");
     l.add("3. spaghetti tacos");
